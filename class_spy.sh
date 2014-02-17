060a7343f14a4bde32a18990b2f183b9327400fd
Keon Vafai <keylime503@gmail.com>
2014-02-16 17:53:07 -0800

    Notifications now work

--- class_spy.sh
+++ class_spy.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-#URL="http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=14S&subareasel=C%26EE+++&idxcrs=0180++++"
-URL="http://vafaikeontest.appspot.com/"
+URL="http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=14S&subareasel=C%26EE+++&idxcrs=0180++++"
 M_TITLE="Message from ClassSpy"
-#M_BODY="C&EE 180 is now open!"
-M_BODY="Did it work?"
+M_BODY="C&EE 180 is now open!"
 
 wget -O page.txt $URL
 md5 page.txt > previous_md5
@@ -17,7 +15,7 @@ do
 	md5 page.txt > last_md5
 	diff previous_md5 last_md5
 	if [ $? -ne 0 ] ; then 
-		terminal-notifier -message $M_BODY -title $M_TITLE
+		terminal-notifier -message "$M_BODY" -title "$M_TITLE"
 	fi
 	mv last_md5 previous_md5
 	rm page.txt
