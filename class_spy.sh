1ca5beac3b38c743a5c3303391e54101f36bf9df
Keon Vafai <keylime503@gmail.com>
2014-02-16 22:10:49 -0800

    Set up as cron job for C&EE 180

--- class_spy.sh
+++ class_spy.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cd /Users/KeyLime503/Documents/Development/ClassSpy/
+
 URL="http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=14S&subareasel=C%26EE+++&idxcrs=0180++++"
+#URL="http://vafaikeontest.appspot.com/"
 M_TITLE="Message from ClassSpy"
-M_BODY="C&EE 180 is now open!"
+M_BODY="C&EE 180 has changed status!"
 
 wget -O page.txt $URL
-md5 page.txt > previous_md5
+md5 page.txt > last_md5
+diff previous_md5 last_md5
+if [ $? -ne 0 ] ; then 
+	terminal-notifier -message "$M_BODY" -title "$M_TITLE"
+fi
+mv last_md5 previous_md5
 rm page.txt
-
-while [ 1 ]
-do
-	sleep 10
-	wget -O page.txt $URL
-	md5 page.txt > last_md5
-	diff previous_md5 last_md5
-	if [ $? -ne 0 ] ; then 
-		terminal-notifier -message "$M_BODY" -title "$M_TITLE"
-	fi
-	mv last_md5 previous_md5
-	rm page.txt
-done
