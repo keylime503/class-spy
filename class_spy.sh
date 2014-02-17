fa5ec567b04e3ba2ddae2505ec0dc0623e194c46
Keon Vafai <keylime503@gmail.com>
2014-02-16 22:50:27 -0800

    Added SMS notification

--- class_spy.sh
+++ class_spy.sh
@@ -8,11 +8,15 @@ URL="http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=14S&subarease
 M_TITLE="Message from ClassSpy"
 M_BODY="C&EE 180 has changed status!"
 
+M_NUM="4084660056"
+M_MESSAGE="C&EE 180 has changed status!"
+
 wget -O page.txt $URL
 md5 page.txt > last_md5
 diff previous_md5 last_md5
 if [ $? -ne 0 ] ; then 
 	terminal-notifier -message "$M_BODY" -title "$M_TITLE"
+	curl "http://textbelt.com/text" -d number=$NUM -d "message=$MESSAGE" 
 fi
 mv last_md5 previous_md5
 rm page.txt
