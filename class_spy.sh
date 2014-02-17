764fc149a2b4dd52b9ff226566826feac13398d7
Keon Vafai <keylime503@gmail.com>
2014-02-16 17:29:12 -0800

    Changed = to -ne

--- class_spy.sh
+++ class_spy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-URL="http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=14S&subareasel=C%26EE+++&idxcrs=0180++++"
+#URL="http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=14S&subareasel=C%26EE+++&idxcrs=0180++++"
+URL="http://vafaikeontest.appspot.com/"
 M_TITLE="Message from ClassSpy"
-M_BODY="C&EE 180 is now open!"
+#M_BODY="C&EE 180 is now open!"
+M_BODY="Did it work?"
 
 wget -O page.txt $URL
 md5 page.txt > previous_md5
@@ -14,8 +16,8 @@ do
 	wget -O page.txt $URL
 	md5 page.txt > last_md5
 	diff previous_md5 last_md5
-	if [ "$?" = "!" ] ; then 
-	      terminal-notifier -message $M_BODY -title $M_TITLE
+	if [ $? -ne 0 ] ; then 
+		terminal-notifier -message $M_BODY -title $M_TITLE
 	fi
 	mv last_md5 previous_md5
 	rm page.txt
